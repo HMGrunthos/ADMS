04ae747d515d8e2b871ce5cc898848cf96e4e290
Sam Hole <sam_hole@hotmail.com>
2016-10-29 19:00:39 +0100

    Expanding message and alarm api

--- ADMSAppControl.ino
+++ ADMSAppControl.ino
@@ -1,6 +1,7 @@
 const unsigned long int INETCONNECTIONTIMEOUT = 5000; // In milliseconds
 const unsigned long int PURCHASETIMEOUT = 30000; // In milliseconds
 const unsigned long int TAUNTTIMEOUT = 1000; // In milliseconds
+const unsigned long int PURCHASECOMPLETETIMEOUT = 30000; // In milliseconds
 
 enum ADMSSystemState {
 	Sleeping,
@@ -12,10 +13,26 @@ enum ADMSSystemState {
 	FailedToFindItem,
 	AlertUserToPurchase,
 	CountdownToPurchase,
-	PurchaseAvoided,
-	SoundPurchaseAlarm,
-	MakePurchase,
+	PurchaseAborted,
+	StartPurchasing,
+	WaitForPurchaseComplete,
 	TauntUser
+};
+
+enum AlarmSound {
+	PurchaseWarningAlarm,
+	AbortedPuchaseAlarm,
+	PuchasingNowAlarm,
+	PuchaseFailedAlarm,
+	PuchaseMadeAlarm
+};
+
+enum DisplayMessage {
+	PurchaseWarningMessage,
+	AbortedPuchaseMessage,
+	PuchasingNowMessage,
+	PuchaseFailedMessage,
+	PuchasedMadeMessage
 };
 
 struct {
@@ -25,8 +42,9 @@ struct {
 		unsigned long int connectToInternet;
 		unsigned long int purchase;
 		unsigned long int taunt;
+		unsigned long int purchaseComplete;
 	} timeouts;
-} appState = {Sleeping, {0, 0, 0}};
+} appState = {Sleeping, {0, 0, 0, 0}};
 
 // Internet functions
 void attemptToConnectToInternet();
@@ -35,6 +53,8 @@ bool hasConnectionToInternetSucceeded();
 // Purchasing functions
 bool findItemToBuy(std::string *itemToPurchase);
 void purchaseSelectedGoods(std::string itemToPurchase);
+bool hasPurchaseSucceeded();
+bool hasPurchaseFailed();
 
 // Phone alert functions
 void sendPhoneAlert();
@@ -42,14 +62,12 @@ void sendPhoneAlert();
 // Button press functions
 bool getButtonPressState();
 
-void soundPurchaseWarningAlarm();
-void soundPuchaseAvoidedAlarm();
-void soundPuchasingAlarm();
-void soundPuchaseMadeAlarm();
+// Alarm sounding function
+void soundAlarm(enum AlarmSound alarm);
 
-void setPurchasingDisplayMessage();
-void setUserAbortedDisplayMessage();
-void setPuchasedDisplayMessage();
+// Display function
+void setDisplayMessage(enum DisplayMessage messageEnum);
+void clearDisplayMessage();
 
 // Utils and hardware management
 static unsigned long int setTimeout(unsigned long int timeout);
@@ -107,39 +125,51 @@ void loop()
 			break;
 		case AlertUserToPurchase:
 			sendPhoneAlert();
-			soundPurchaseWarningAlarm();
+			soundAlarm(PurchaseWarningAlarm);
+			setDisplayMessage(PurchaseWarningMessage);
 			appState.timeouts.purchase = setTimeout(PURCHASETIMEOUT);
 			appState.systemState = CountdownToPurchase;
 			break;
 		case CountdownToPurchase:
 			if(getButtonPressState()) {
-				appState.systemState = PurchaseAvoided;
+				appState.systemState = PurchaseAborted;
 			} else { // Button not pressed before timed out
 				if(hasTimedOut(appState.timeouts.purchase)) { // Test the purchase timeout
 					// No button pressed so start making a purchase
-					appState.systemState = SoundPurchaseAlarm;
+					appState.systemState = StartPurchasing;
 				} else {
 					appState.systemState = CountdownToPurchase; // Stay in the same state
 				}
 			}
 			break;
-		case PurchaseAvoided:
-			setUserAbortedDisplayMessage();
-			soundPuchaseAvoidedAlarm();
+		case PurchaseAborted:
+			soundAlarm(AbortedPuchaseAlarm);
+			setDisplayMessage(AbortedPuchaseMessage);
 			appState.timeouts.taunt = setTimeout(TAUNTTIMEOUT);
 			appState.systemState = TauntUser;
 			break;
-		case SoundPurchaseAlarm:
-			setPurchasingDisplayMessage();
-			soundPuchasingAlarm();
-			appState.systemState = MakePurchase;
-			break;
-		case MakePurchase:
+		case StartPurchasing:
+			soundAlarm(PuchasingNowAlarm);
+			setDisplayMessage(PuchasingNowMessage);
 			purchaseSelectedGoods(appState.itemToPurchase);
-			setPuchasedDisplayMessage();
-			soundPuchaseMadeAlarm();
+			appState.timeouts.purchaseComplete = setTimeout(PURCHASECOMPLETETIMEOUT);
+			appState.systemState = WaitForPurchaseComplete;
+			break;
+		case WaitForPurchaseComplete:
+			if(hasPurchaseSucceeded()) {
+				soundAlarm(PuchaseMadeAlarm);
+				setDisplayMessage(PuchasedMadeMessage);
+				appState.systemState = TauntUser;
+			} else if(hasPurchaseFailed()){
+				soundAlarm(PuchaseFailedAlarm);
+				setDisplayMessage(PuchaseFailedMessage);
+				appState.systemState = TauntUser;
+			} else if(hasTimedOut(appState.timeouts.purchaseComplete)) {
+				soundAlarm(PuchaseFailedAlarm);
+				setDisplayMessage(PuchaseFailedMessage);
+				appState.systemState = TauntUser;
+			}
 			appState.timeouts.taunt = setTimeout(TAUNTTIMEOUT);
-			appState.systemState = TauntUser;
 			break;
 		case TauntUser:
 			if(hasTimedOut(appState.timeouts.taunt)) { // Finished taunting the user
@@ -173,9 +203,48 @@ static bool hasTimedOut(unsigned long int timer, unsigned long int currentTime)
 
 static void powerDownPeripherals()
 {
+	clearDisplayMessage();
 }
 
 static bool timeToWakeUp()
 {
 	return true;
+}
+
+// Alarm sounding function
+void soundAlarm(enum AlarmSound alarm)
+{
+	switch(alarm) {
+		case PurchaseWarningAlarm:
+			break;
+		case AbortedPuchaseAlarm:
+			break;
+		case PuchasingNowAlarm:
+			break;
+		case PuchaseFailedAlarm:
+			break;
+		case PuchaseMadeAlarm:
+			break;
+	}
+}
+
+// Display function
+void setDisplayMessage(enum DisplayMessage message)
+{
+	switch(message) {
+		case PurchaseWarningMessage:
+			break;
+		case AbortedPuchaseMessage:
+			break;
+		case PuchasingNowMessage:
+			break;
+		case PuchaseFailedMessage:
+			break;
+		case PuchasedMadeMessage:
+			break;
+	}
+}
+
+void clearDisplayMessage()
+{
 }
