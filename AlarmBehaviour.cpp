68cfa3462db6476dedaf6910d9709a818db5bb1d
john <john.welford@gmail.com>
2016-10-30 12:51:52 +0000

    added some dependencies

--- AlarmBehaviour.cpp
+++ AlarmBehaviour.cpp
@@ -1,20 +1,22 @@
 #include "AlarmBehaviour.h"
 // This #include statement was automatically added by the Particle IDE.
-#include "SparkIntervalTimer/SparkIntervalTimer.h"
+#include "SparkIntervalTimer.h"
 #include "math.h"
 
 // connect a 8Ohm speaker with a 220Ohm resistor to pin DAC and GND
-const int audio = DAC1;
-const int speakerUpdate = 50; // uSecs
+const int AUDIO = DAC1;
+const int SPEAKERUPDATE = 50; // uSecs
 IntervalTimer audio_clock;
+int wave_ix = 0;
+int currentFreq = 0;
 
 static void play_wave();
-static void start_play();
+static void start_play(int freq);
 static void stop_play();
 
 void initAlarm()
 {
-	pinMode(audio, OUTPUT);
+	pinMode(AUDIO, OUTPUT);
 }
 
 // Alarm sounding function
@@ -37,18 +39,19 @@ void soundAlarm(enum AlarmSound alarm)
 	}
 }
 
-void play_wave(int freq) {
-    // wave_ix++;
-    analogWrite(audio, (2046*sin(freq*2*M_PI*wave_ix*speakerUpdate*1e-6)+2046));
+void play_wave(void) {
+    wave_ix++;
+    analogWrite(AUDIO, (2046*sin(currentFreq*2*M_PI*wave_ix*SPEAKERUPDATE*1e-6)+2046));
 }
 
 void start_play(int freq) {
-    // wave_ix = 0;
-    audio_clock.begin(play_wave(freq)), speakerUpdate, uSec);
+    wave_ix = 0;
+		currentFreq = freq;
+    audio_clock.begin(play_wave, SPEAKERUPDATE, uSec);
 }
 
 void stop_play(void) {
     audio_clock.end();
-    analogWrite(audio, 2048);
-    // wave_ix = 0;
+    analogWrite(AUDIO, 0);
+    wave_ix = 0;
 }
