172dfe10a6e044a51f4054b182d3f1a5889eb7b0
joe <joeh4444@gmail.com>
2016-10-30 07:35:05 +0000

    refractoring

--- webhook-test.ino
+++ webhook-test.ino
@@ -1,56 +1,29 @@
 #include "Arduino.h"
 #include "sha256.h"
 #include "Base64.h"
-
-char key[] = "yAiOLqFITOmF8MUE3UnLSQfNrexSNPGVysRAFtB1";
+#include "secret.h"
 
 char orderId[256] = "";
 int state = 0;
 
-void setup() {
-  Serial.begin(9600);
-  // Subscribe to the integration response event
-  Particle.subscribe("hook-response/zinc-purchase", getOrderID, MY_DEVICES);
-  Particle.subscribe("hook-response/zinc-checkorder", getOrderStatus, MY_DEVICES);
-
-
-  delay (10000);
-
-  // Header, Timestamps and query
-  std::string header = "GET\nwebservices.amazon.co.uk\n/onca/xml\n";
-  std::string timestamp = (std::string)Time.format("%Y-%m-%dT%H%%3A%M%%3A%SZ");
-  std::string timestampURL = (std::string)Time.format("%Y-%m-%dT%H:%M:%SZ");
-  std::string query = "AWSAccessKeyId=AKIAIYD5UUEI3ZPSC2NQ&AssociateTag=a052260-21&Keywords=particle&Operation=ItemSearch&ResponseGroup=ItemAttributes&SearchIndex=Electronics&Service=AWSECommerceService&Sort=price";
-  query += "&Timestamp=" + timestamp ;
-
-  // String to Sign
-  std::string tosign = header + query;
-  /*Serial.print("tosign result: ");
-  Serial.println(tosign.c_str());*/
-
-  // SHA256 with HMAC
-  Sha256.initHmac((uint8_t*)key, strlen(key));
-  Sha256.print(tosign.c_str());
-
-  // Get SHA256 result
-  char* sign = (char*) Sha256.resultHmac();
-
-  // BASE64 Encode
-  int signLen = 32;
-  int encodedSignLen = base64_enc_len(signLen);
-  char encodedSign[encodedSignLen];
-  base64_encode(encodedSign, sign, signLen);
-  /*Serial.print("sign   result: ");
-  Serial.println(encodedSign);*/
-
-  // Data to pulish
-  char data[256];
-  snprintf(data, sizeof(data), "{\"timestamp\":\"%s\", \"signature\":\"%s\"}", timestampURL.c_str(), encodedSign);
-  Particle.publish("amazon-search", data, PRIVATE);
-
+void getProductId(const char *event, const char *data) {
+  std::string temp = (std::string)xmlTakeParam(data, "ParentASIN");
+  if(strcmp(temp.c_str(), "") != 0) {
+    Serial.printlnf("Application>\tProductId Found=%s", temp.c_str());
+  }
 }
 
-void getOrderID(const char *event, const char *data) {
+String xmlTakeParam(String inStr,String needParam) {
+  if(inStr.indexOf("<"+needParam+">")>0){
+     int CountChar=needParam.length();
+     int indexStart=inStr.indexOf("<"+needParam+">");
+     int indexStop= inStr.indexOf("</"+needParam+">");
+     return inStr.substring(indexStart+CountChar+2, indexStop);
+  }
+  return "";
+}
+
+void getOrderId(const char *event, const char *data) {
   // Debug
   //Serial.printlnf("Order ID Received=%s", data);
 
@@ -93,6 +66,83 @@ void getOrderStatus(const char *event, const char *data) {
 
 }
 
+void purchaseSelectedGoods(const char* productId) {
+  // Start Delay
+  Serial.println("Application>\tWaiting 30s to purchase");
+  delay(10000);
+  // Start Delay
+  Serial.println("Application>\tWaiting 20s to purchase");
+  delay(10000);
+  // Start Delay
+  Serial.println("Application>\tWaiting 10s to purchase");
+  delay(10000);
+
+  // Trigger the purchase
+  Serial.println("Application>\tPurchasing...");
+  Particle.publish("zinc-purchase", productId, PRIVATE);
+  state = 1;
+}
+
+void requestAmazonSearch(std::string keywords, std::string searchIndex) {
+  delay(10000);
+  Serial.println("Application>\tRequesting an amazon search...");
+  // Header, Timestamps and query
+  std::string header = "GET\nwebservices.amazon.co.uk\n/onca/xml\n";
+  std::string timestamp = (std::string)Time.format("%Y-%m-%dT%H%%3A%M%%3A%SZ"); // Needs to be as %3a for signing
+  std::string timestampURL = (std::string)Time.format("%Y-%m-%dT%H:%M:%SZ");    // Send with : to Webhook
+  // Changes to the query string below MUST be replicated in the Webhook
+  // Query MUST be in Alphabetical Order
+  std::string query =
+    AWSSKEY + "&Keywords=" + keywords +
+    "&Operation=ItemSearch&ResponseGroup=ItemAttributes&SearchIndex=" +
+    searchIndex + "&Service=AWSECommerceService&Sort=price&Timestamp=" +
+    timestamp;
+
+  // String to Sign
+  std::string tosign = header + query;
+  /*Serial.print("tosign result: ");
+  Serial.println(tosign.c_str());*/
+
+  // SHA256 with HMAC
+  Sha256.initHmac((uint8_t*)AWSSECRET.c_str(), AWSSECRET.length());
+  Sha256.print(tosign.c_str());
+
+  // Get SHA256 result
+  char* sign = (char*) Sha256.resultHmac();
+
+  // BASE64 Encode
+  int signLen = 32;
+  int encodedSignLen = base64_enc_len(signLen);
+  char encodedSign[encodedSignLen];
+  base64_encode(encodedSign, sign, signLen);
+  /*Serial.print("sign   result: ");
+  Serial.println(encodedSign);*/
+
+  // Data to pulish
+  char data[256];
+  snprintf(data, sizeof(data),
+    "{\"timestamp\":\"%s\","
+    " \"signature\":\"%s\","
+    " \"keywords\":\"%s\","
+    " \"searchIndex\":\"%s\"}",
+    timestampURL.c_str(),
+    encodedSign,
+    keywords.c_str(),
+    searchIndex.c_str());
+  Particle.publish("amazon-search", data, PRIVATE);
+}
+
+void setup() {
+  Serial.begin(9600);
+  // Subscribe to the integration response event
+  Particle.subscribe("hook-response/zinc-purchase", getOrderId, MY_DEVICES);
+  Particle.subscribe("hook-response/zinc-checkorder", getOrderStatus, MY_DEVICES);
+  Particle.subscribe("hook-response/amazon-search", getProductId, MY_DEVICES);
+
+  requestAmazonSearch("iPhone", "Electronics");
+
+}
+
 void loop() {
 
   switch(state) {
@@ -119,21 +169,4 @@ void loop() {
       break;
   }
   delay(5000);
-}
-
-void purchaseSelectedGoods(const char* productId) {
-  // Start Delay
-  Serial.println("Application>\tWaiting 30s to purchase");
-  delay(10000);
-  // Start Delay
-  Serial.println("Application>\tWaiting 20s to purchase");
-  delay(10000);
-  // Start Delay
-  Serial.println("Application>\tWaiting 10s to purchase");
-  delay(10000);
-
-  // Trigger the purchase
-  Serial.println("Application>\tPurchasing...");
-  Particle.publish("zinc-purchase", productId, PRIVATE);
-  state = 1;
 }
